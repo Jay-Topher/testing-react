847a6f9d3aa85be10ca62578ece9b6ffdca68d9a
Jones Ogolo <jay.topher11@gmail.com>
2020-10-15 08:51:06 +0100

    Testing 7 - 10

---
+++ src/__tests__/calculator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import Calculator from '../calculator'
+
+expect.extend(jestDOM)
+
+test('renders', () => {
+  const {getByText} = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
+})
